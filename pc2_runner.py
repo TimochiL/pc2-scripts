a0533b9b12f552a69af7e66a188eb55fb75e7e23
Timothy Liu <timothy.l.6uy@gmail.com>
2023-03-25 15:31:26 -0500

    Implemented server configuration options

--- pc2_runner.py
+++ pc2_runner.py
@@ -17,18 +17,70 @@ class pc2_instance:
         self.admin_proc = None
         self.started = False
         
-        self.ether_is_connected = False
+        self.wiFi_not_DHCP = False
+        self.ether_not_DHCP = False
         self.ether_full_name = ''
     
     def get_help(self):
         print("Ensure that 'pc2_runner.py' is executed with administrator\naccess on a Windows OS\n\n")
         print("To start 'Server' and 'Admin', enter 'start'\n")
         print("To open any PC2 batch file, enter the name of the file \nwithout the leading 'PC2' and trailing '.bat' extension\n")
-        print("To transfer contest from 'wi-Fi' to Ethernet connection,\nenter '/c ethernet'\n")
+        print("To transfer contest from 'wi-Fi' to Ethernet connection,\nenter '/ethernet'\n")
+        print("To set server configuration, enter '/set ini'\n")
         print("To end the script, enter 'exit'\n")
     
     def set_working_path(self, path_name):
         os.chdir(path_name)
+        
+    def set_ini(self):
+        #print("\nCurrent server configuration: '"+self.get_ini_config()+"'")
+        self.get_ini_config()
+        print("Choose server [ localhost / network ]")
+        
+        output = ''
+        index = 0
+        ini_path = os.path.join(self.bin_path, 'pc2v9.ini')
+        # write_ini_path = os.path.join(self.bin_path, 'pc2v9_temp.ini')
+        serv = None
+        while serv is None:
+            serv = input('>>> ').strip().lower()
+            match serv:
+                case 'localhost':
+                    serv = 'localhost'
+                case 'network':
+                    serv = '192.168.1.2'
+                case default:
+                    print("Invalid server option. Enter 'localhost' or 'network'.\n")
+                    serv = None
+        
+        # Options: default path / set path
+        with open(ini_path, "r+") as f:
+            for line in f:
+                if index < 12 or index > 12:
+                    output += str(line)
+                else:
+                    output += 'server='+serv+':50002\n'
+                index += 1
+            f.seek(0)
+            f.write(output)
+            f.truncate()
+            
+        print("Server set to '"+serv+"'.\n")
+        
+    def get_ini_config(self):
+        serv = ''
+        ini_path = os.path.join(self.bin_path, 'pc2v9.ini')
+        index = 0
+        
+        f =  open(ini_path, "r")
+        for line in f:
+            if index == 12:
+                serv = str(line)[7:-7]
+            index += 1
+        f.close()
+        
+        print("\nCurrent server:",serv)
+            
         
     def get_profiles(self):
         prof_path = os.path.join(self.bin_path, 'profiles')
@@ -42,6 +94,7 @@ class pc2_instance:
         
         self.toggle_firewall()
         self.yn_command('netsh interface ip set address name="wi-Fi" static 192.168.1.2 255.255.255.0 192.168.1.2')
+        self.wiFi_not_DHCP = True
         
         self.server_proc = subprocess.Popen(
             (server_path),
@@ -63,6 +116,7 @@ class pc2_instance:
         if 'pc2'+other+'.bat' in os.listdir(self.bin_path):
             other_path = os.path.join(self.bin_path, 'pc2'+other+'.bat')
             subprocess.Popen((other_path))
+            print("Running '"+"pc2"+other+".bat'.")
         else:
             print("Command '"+other+"' does not exist.")
         
@@ -79,7 +133,7 @@ class pc2_instance:
                     print("Process executed.\n")
                     done = True
                 case 'n':
-                    print("Cancelling call to '"+cmd+"'.")
+                    print("Cancelling call to '"+cmd+"'.\n")
                     done = True
                 case default:
                     print("Invalid response.")
@@ -113,13 +167,13 @@ class pc2_instance:
             else:
                 print("Invalid response.")
              
-    def transfer_to_ethernet(self):
+    def transfer_to_ethernet(self): 
         print('Ensure Ethernet is connected\n')
         done = False
         valid_IP = False
         
         while not done:
-            print("Transfer contest onto 'Ethernet' connection [y/n]")
+            print("Transfer contest onto Ethernet connection [y/n]")
             
             response = input('>>> ').strip().lower()
             match response:
@@ -140,7 +194,7 @@ class pc2_instance:
                             if host_ID != 'string' and host_ID > 0:
                                 valid_IP = True
                             else:
-                                print('Invalid IP host ID. Enter a valid number greater than 0.')
+                                print('\nInvalid IP host ID. Enter a valid number greater than 0.')
                                 print('Ensure that host ID is available.\n')
                               
                         subprocess.run('netsh interface ip set address name="wi-Fi" static 192.168.1.'+str(host_ID)+' 255.255.255.0 192.168.1.'+str(host_ID))
@@ -148,7 +202,7 @@ class pc2_instance:
                         subprocess.run('netsh interface ip set address name="wi-Fi" dhcp')
                         
                         print("Transfer to",ether_name,"successful.")
-                        self.ether_is_connected = True
+                        self.ether_not_DHCP = True
                         
                     done = True
                     
@@ -158,7 +212,7 @@ class pc2_instance:
                     
                 case default:
                     print("Invalid response.")
-                    print("Enter 'y' to transfer contest to 'Ethernet'.\nEnter 'n' to stay on 'wi-Fi' connection")
+                    print("Enter 'y' to transfer contest to Ethernet.\nEnter 'n' to stay on 'wi-Fi' connection")
 
     def check_ether_connection(self):
         etherConnected = False
@@ -197,24 +251,39 @@ class pc2_instance:
         if len(etherConnections) == 0:
             print("No Ethernet ports found. Stay on wi-Fi.")
         else:
-            print("Enter '/<#>' to choose an Ethernet connection to transfer the contest to:")
+            print("Enter '/<#>' to choose an Ethernet connection to transfer the contest to:\n")
             
             index = 1
             for e in etherConnections:
-                print("\t/"+str(index),e[0],':',e[1])
+                str_btwn = ''
+                len_btwn = 58 - len(e[0]) - len(e[1]) + 1
+                
+                spc_first = False
+                if len(e[0]) % 2 == 0:
+                    spc_first = True
+                for integer in range(len(e[0]),len(e[0])+len_btwn):
+                    add_val = '.'
+                    if integer % 2 == 0:
+                        add_val = ' '
+                    str_btwn += add_val
+                print("     /"+str(index),e[0]+str_btwn+e[1])
                 index+=1
             print()
             
             while not etherConnected:
-                response = input('>>> ').strip()[1:]
+                response = input('>>> ').strip()
+                if '/' not in response:
+                    response = 'str'
+                else:
+                    response = response[1:]
                 try:
                     response = int(response)
                     if response <= 0 or response > len(etherConnections):
-                        print("Please choose one of the above options.")
+                        print("Invalid Response. Enter '/<#>' based on above options.")
                     else:
                         ether_name = etherConnections[response-1][0]
                         ether_address = etherConnections[response-1][1]
-                        response2 = None
+                        response2 = None    
                         
                         print("Current status of Ethernet adapter",ether_name+":",ether_address)
                         print("Confirm contest transfer to '"+ether_name+" : "+ether_address+"' [y/n]")
@@ -224,14 +293,14 @@ class pc2_instance:
                             match response2:
                                 case 'y':
                                     self.ether_full_name = ether_name
+                                    etherConnected = True
                                 case 'n':
-                                    return 'exit'
+                                    ether_name = 'exit'
+                                    etherConnected = True
                                 case default:
                                     print("Enter 'y' to confirm transfer or 'n' to cancel.")
                                     response2 = None
-                                    
-                    etherConnected = True
-                    
+        
                 except ValueError:
                     print("Invalid Response. Enter '/<#>' based on above options.")
         
@@ -241,7 +310,9 @@ class pc2_instance:
 def main():
     pc2i = pc2_instance()
     command = ''
-    print("Enter '/h' for help.")
+    pc2i.set_ini()
+    
+    print("\nEnter '/h' for help.")
     
     while command != 'exit':
         command = input(">>> ").strip().lower()
@@ -249,14 +320,17 @@ def main():
             case "start":
                 pc2i.start_contest()
             case "exit":
-                if pc2i.started:
+                if pc2i.wiFi_not_DHCP:
                     pc2i.yn_command('netsh interface ip set address name="wi-Fi" dhcp')
-                    if pc2i.ether_is_connected:
-                        pc2i.yn_command('netsh interface ip set address name="'+pc2i.ether_full_name+'" dhcp')
+                if pc2i.ether_not_DHCP:
+                    pc2i.yn_command('netsh interface ip set address name="'+pc2i.ether_full_name+'" dhcp')
+                if pc2i.started:
                     pc2i.toggle_firewall()
                 print("'pc2_runner.py' terminated")
-            case "/c ethernet":
+            case "/ethernet":
                 pc2i.transfer_to_ethernet()
+            case "/set ini":
+                pc2i.set_ini()
             case "/h":
                 pc2i.get_help()
             case default:
